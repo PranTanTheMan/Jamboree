ffe08c4bc45841f2f79c24d3d557f98891ba02a7
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-08-06 23:05:53 +0000

    getting finished with hero and nabber

--- pages/index.js
+++ pages/index.js
@@ -11,46 +11,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <ul>
+        <h1>Jamboree</h1>
+        <li><a class="special-button"href="#">Open Chatroom</a></li>
+        <li><a href="#">Contact</a></li>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <li><a href="#">Features</a></li>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      </ul>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <div class="hero">
+        <div class="sub-hero">
+        <h1>One time, never again</h1>
+        <h2> Say what's on your mind</h2>
+          <br></br>
+        <a>Start Chatting</a>
+          </div>
+      </div>
 
       <footer className={styles.footer}>
         <a
@@ -58,10 +36,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Copyright © 2022 Jamboree, All Rights Reserved
+
         </a>
       </footer>
     </div>
