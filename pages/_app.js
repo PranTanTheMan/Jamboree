99c1c9546f28feb204ca9f51c2c108a90a8293b7
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2022-08-07 02:59:38 +0000

    IUdk

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.css'
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../styles/theme";
-import Fonts from "../components/font";
+
 
 function MyApp({ Component, pageProps }) {
-  return <ChakraProvider theme><Component {...pageProps} /></ChakraProvider>
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
